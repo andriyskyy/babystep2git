c61865ae34fabd9d4d633f9d0f4ce47bfdaeb8f0
andriyskyy <71843396+andriyskyy@users.noreply.github.com>
2020-12-06 20:31:34 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,3 +2,8 @@ provider "aws" {
    profile    = "default"
    region     = "us-east-2"
  }
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = "https://api.datadoghq.eu/"
+}
