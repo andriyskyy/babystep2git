bd9c84564e4b560e92612e8f7ed6a38a14cf6f1a
andriyskyy <71843396+andriyskyy@users.noreply.github.com>
2020-12-07 12:28:46 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,16 @@ variable "datadog_app_key" {
 variable "cluster_name" {
   default     = "NextCloud_Demo"
   description = "EKS cluster name"
+}
+variable "node_group_name"
+  default     = "Nextcloud_node"
+  description = "Worker node group name"
+}
+variable "tag_name" {
+  default     = "Nextcloud"
+  description = "Cluster tag name"
+}
+variable "ssh_keyname"
+  default     = "AWS_key"
+  description = "SSH keypair name for remote access"
+}
