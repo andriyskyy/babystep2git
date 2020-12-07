09968ed8221c55c0b793ee2f17deac0a4b0e726e
andriyskyy <71843396+andriyskyy@users.noreply.github.com>
2020-12-07 12:20:42 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
 variable "datadog_api_key" {
   default     = ""
+  description = "Datadog API KEY"
 }
 variable "datadog_app_key" {
   default     = ""
+  description = "Datadog APP KEY"
 }
+variable "cluster_name" {
+  default     = "NextCloud_Demo"
+  description = "EKS cluster name"
