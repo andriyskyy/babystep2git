f9f72170183664a1a6065bbaa80239c1f48241f9
andriyskyy <71843396+andriyskyy@users.noreply.github.com>
2020-12-07 15:10:27 +0200

    Delete provider.tf

--- provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-   version    = ">= 2.28.1"
-   region     = var.region
- }
-provider "datadog" {
-  api_key = var.datadog_api_key
-  app_key = var.datadog_app_key
-  api_url = var.api_url
-}
