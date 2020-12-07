19b96cf4e61a1f32167851ea8dd815a113a96e07
andriyskyy <71843396+andriyskyy@users.noreply.github.com>
2020-12-07 12:31:40 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "cluster_name" {
   default     = "NextCloud_Demo"
   description = "EKS cluster name"
 }
-variable "node_group_name"
+variable "node_group_name" {
   default     = "Nextcloud_node"
   description = "Worker node group name"
 }
@@ -18,7 +18,7 @@ variable "tag_name" {
   default     = "Nextcloud"
   description = "Cluster tag name"
 }
-variable "ssh_keyname"
+variable "ssh_keyname" {
   default     = "AWS_key"
   description = "SSH keypair name for remote access"
 }
